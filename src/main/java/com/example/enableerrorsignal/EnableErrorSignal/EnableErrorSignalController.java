12d3ad72b8f72624f5c9ed5329ba5d70f1e834e1
sarikachawla <sarika.chawla@rabobank.nl>
2025-03-12 14:12:40 +0100

    Removed annotation files

--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/EnableErrorSignalController.java
+++ src/main/java/com/example/enableerrorsignal/EnableErrorSignal/EnableErrorSignalController.java
@@ -1,6 +1,7 @@
 package com.example.enableerrorsignal.EnableErrorSignal;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,9 +10,9 @@ public class EnableErrorSignalController {
 
     @GetMapping("/monitoring/{id}")
 
-    public String ledstatus(@PathVariable String id , Model model) {
-        model.addAttribute("id", ledId);
-        return "Here is the status of LED with id" + ledId;
+    public String ledstatus(@PathVariable String id ) {
+        //model.addAttribute("id", ledId);
+        return "Here is the status of LED with id " + id;
     }
 
     @GetMapping("/monitoring/{ledId}/ON")
--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/RestController.java
+++
@@ -1,4 +0,0 @@
-package com.example.enableerrorsignal.EnableErrorSignal;
-
-public @interface RestController {
-}
