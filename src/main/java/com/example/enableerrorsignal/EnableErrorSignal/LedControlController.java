c649591eabaab7cfc15ae56c5618e716c0c364e3
sarikachawla <sarika.chawla@rabobank.nl>
2025-04-03 11:43:18 +0200

    added controller for pi4j

--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/LedControlController.java
+++ src/main/java/com/example/enableerrorsignal/EnableErrorSignal/LedControlController.java
@@ -1,55 +1,33 @@
-package com.example.enableerrorsignal.EnableErrorSignal;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import spark.Spark;
 
-import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
-import com.pi4j.io.gpio.digital.DigitalState;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class LedControlController {
+    public static void main(String[] args) {
+        final GpioController gpio = GpioFactory.getInstance();
+        final GpioPinDigitalOutput redLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "RedLED", PinState.LOW);
+        final GpioPinDigitalOutput greenLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "GreenLED", PinState.LOW);
 
-    private final Context pi4j;
-    private final DigitalOutput redLed;
-    private final DigitalOutput greenLed;
+        Spark.get("/light/:color", (req, res) -> {
+            String color = req.params(":color");
+            if ("red".equalsIgnoreCase(color)) {
+                redLed.high();
+                greenLed.low();
+                return "Red light is ON";
+            } else if ("green".equalsIgnoreCase(color)) {
+                greenLed.high();
+                redLed.low();
+                return "Green light is ON";
+            } else {
+                redLed.low();
+                greenLed.low();
+                return "Invalid color. Both lights are OFF";
+            }
+        });
 
-    public LedControlController() {
-        // Create Pi4J context
-        this.pi4j = Pi4J.newAutoContext();
-
-        // Provision GPIO pins for the LEDs
-        DigitalOutputConfigBuilder redLedConfig = DigitalOutput.newConfigBuilder(pi4j)
-                .id("red-led")
-                .name("Red LED")
-                .address(1) // GPIO pin number for red LED
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW);
-
-        DigitalOutputConfigBuilder greenLedConfig = DigitalOutput.newConfigBuilder(pi4j)
-                .id("green-led")
-                .name("Green LED")
-                .address(2) // GPIO pin number for green LED
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW);
-
-        this.redLed = pi4j.create(redLedConfig);
-        this.greenLed = pi4j.create(greenLedConfig);
-    }
-
-    @GetMapping("/led/{color}/{state}")
-    public String controlLed(@PathVariable String color, @PathVariable String state) {
-        DigitalOutput led = "red".equalsIgnoreCase(color) ? redLed : greenLed;
-        if ("on".equalsIgnoreCase(state)) {
-            led.high();
-            return color + " LED is ON";
-        } else if ("off".equalsIgnoreCase(state)) {
-            led.low();
-            return color + " LED is OFF";
-        } else {
-            return "Invalid state. Use 'on' or 'off'.";
-        }
+        Spark.init();
     }
 }
