9335f36b734dc4c2ab0c4ef3c826ad7332b78f45
sarikachawla <sarika.chawla@rabobank.nl>
2025-04-22 14:37:16 +0200

    added raspberry pi4j code for led control

--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/LedControlController.java
+++ src/main/java/com/example/enableerrorsignal/EnableErrorSignal/LedControlController.java
@@ -1,33 +1,65 @@
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
-import spark.Spark;
+package com.example.enableerrorsignal.EnableErrorSignal;
+
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.util.Console;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 public class LedControlController {
-    public static void main(String[] args) {
-        final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput redLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "RedLED", PinState.LOW);
-        final GpioPinDigitalOutput greenLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "GreenLED", PinState.LOW);
 
-        Spark.get("/light/:color", (req, res) -> {
-            String color = req.params(":color");
-            if ("red".equalsIgnoreCase(color)) {
-                redLed.high();
-                greenLed.low();
-                return "Red light is ON";
-            } else if ("green".equalsIgnoreCase(color)) {
-                greenLed.high();
-                redLed.low();
-                return "Green light is ON";
-            } else {
-                redLed.low();
-                greenLed.low();
-                return "Invalid color. Both lights are OFF";
+    // Connect a button to PIN 18 = BCM 24
+    private static final int PIN_BUTTON = 24;
+    // Connect a LED to PIN 15 = BCM 22
+    private static final int PIN_LED = 22;
+
+    private static int pressCount = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        final var console = new Console();
+
+        var pi4j = Pi4J.newAutoContext();
+
+        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id("led")
+                .name("LED Flasher")
+                .address(PIN_LED)
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider("pigpio-digital-output");
+        var led = pi4j.create(ledConfig);
+
+        var buttonConfig = DigitalInput.newConfigBuilder(pi4j)
+                .id("button")
+                .name("Press button")
+                .address(PIN_BUTTON)
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(3000L)
+                .provider("pigpio-digital-input");
+        var button = pi4j.create(buttonConfig);
+        button.addListener(e -> {
+            if (e.state() == DigitalState.LOW) {
+                pressCount++;
+                console.println("Button was pressed for the " + pressCount + "th time");
             }
         });
 
-        Spark.init();
+        while (pressCount < 5) {
+            if (led.equals(DigitalState.HIGH)) {
+                console.println("LED low");
+                led.low();
+            } else {
+                console.println("LED high");
+                led.high();
+            }
+            Thread.sleep(500 / (pressCount + 1));
+        }
+
+        pi4j.shutdown();
     }
 }
