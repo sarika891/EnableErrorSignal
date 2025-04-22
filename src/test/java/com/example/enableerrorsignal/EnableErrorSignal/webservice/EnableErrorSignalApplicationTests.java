030ec25ba5e94991057a5fac88f3e6f0cbbd37d0
sarikachawla <sarika.chawla@rabobank.nl>
2025-04-23 00:39:58 +0200

    move test file to webservice package

--- src/test/java/com/example/enableerrorsignal/EnableErrorSignal/webservice/EnableErrorSignalApplicationTests.java
+++ src/test/java/com/example/enableerrorsignal/EnableErrorSignal/webservice/EnableErrorSignalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.enableerrorsignal.EnableErrorSignal;
+package com.example.enableerrorsignal.EnableErrorSignal.webservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
