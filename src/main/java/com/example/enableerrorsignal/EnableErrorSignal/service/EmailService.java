a9cacf578b97b894cb5d8741ea15f39cfc014ea9
marian.szabo@rabobank.nl <marian.szabo@rabobank.nl>
2025-06-27 22:09:44 +0200

    The lights are triggered based on the presence of an email with the subject Alert in the past 5 minutes

--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/service/EmailService.java
+++ src/main/java/com/example/enableerrorsignal/EnableErrorSignal/service/EmailService.java
@@ -1,63 +1,75 @@
 package com.example.enableerrorsignal.EnableErrorSignal.service;
 
-import com.sun.mail.imap.IMAPFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.mail.*;
-import javax.mail.event.MessageCountEvent;
-import javax.mail.event.MessageCountListener;
+import jakarta.mail.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Properties;
 
 @Service
 public class EmailService {
 
-    private static final Logger log = LoggerFactory.getLogger(EmailReaderScheduler.class);
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
     private final GpioServiceInterface gpioService;
-    private final Store emailReadingClient;
 
-    public EmailService(GpioServiceInterface gpioService) throws MessagingException {
+    public EmailService(GpioServiceInterface gpioService) {
         this.gpioService = gpioService;
-        this.emailReadingClient = getEmailReadingClient();
-        this.emailReadingClient.connect();
     }
 
-    private Store getEmailReadingClient() throws NoSuchProviderException {
-        return getAuthenticationSession().getStore("imaps");
-    }
-
-    public void listenForNewEmails() throws MessagingException {
-        log.info("Fetch emails....");
-        if (!emailReadingClient.isConnected()) {
-            emailReadingClient.connect();
+    public void checkEmail() {
+        log.info("===========================================");
+        log.info("Checking email...");
+        try {
+            Store emailReadingClient = getEmailReadingClient();
+            Folder inbox = getInboxFolder(emailReadingClient);
+            Message[] messages = inbox.getMessages();
+            if (didAnyErrorOccurred(messages)) {
+                gpioService.turnOnRedLight();
+                gpioService.turnOffGreenLight();
+                log.info("At least one error occurred");
+            } else {
+                gpioService.turnOffRedLight();
+                gpioService.turnOnGreenLight();
+                log.info("No error occurred");
+            }
+            inbox.close(false);
+            emailReadingClient.close();
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
-        Folder folder = emailReadingClient.getFolder("INBOX");
-        if (folder instanceof IMAPFolder) {
-            IMAPFolder inbox = (IMAPFolder) folder;
-            inbox.open(Folder.READ_WRITE);
-
-            inbox.addMessageCountListener(new MessageCountListener() {
-                @Override
-                public void messagesAdded(MessageCountEvent event) {
-                    for (Message message : event.getMessages()) {
-                        try {
-                            log.info("New Email Subject: {}", message.getSubject());
-                            gpioService.turnOnRedLight();
-                            log.info("Red light blink method triggered.");
-                        } catch (MessagingException e) {
-                            log.error("Error reading email subject: {}", e.getMessage());
-                        }
-                    }
-                }
-
-                @Override
-                public void messagesRemoved(MessageCountEvent event) {
-                    // Handle message removal if needed
-                }
-            });
-        }
+        log.info("===========================================");
     }
+
+    private boolean didAnyErrorOccurred(Message[] messages) throws MessagingException {
+        boolean anErrorOccurred = false;
+        for (Message message : messages) {
+            if (isMessageRecent(message.getSentDate()) && message.getSubject().contains("Alert")) {
+                anErrorOccurred = true;
+            }
+        }
+        return anErrorOccurred;
+    }
+
+    private boolean isMessageRecent(Date sentDate) {
+        return new Date().toInstant().minus(5, ChronoUnit.MINUTES)
+                .isBefore(sentDate.toInstant());
+    }
+
+    private Store getEmailReadingClient() throws MessagingException {
+        Store store = getAuthenticationSession().getStore("imaps");
+        store.connect();
+        return store;
+    }
+
+    private static Folder getInboxFolder(Store store) throws MessagingException {
+        Folder inbox = store.getFolder("INBOX");
+        inbox.open(Folder.READ_ONLY);
+        return inbox;
+    }
+
 
     private Session getAuthenticationSession() {
         String host = "mail.mailo.com";
