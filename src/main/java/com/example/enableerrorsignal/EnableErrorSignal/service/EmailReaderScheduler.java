107b94f5bbc3461bb3dae2c112b5906b5279d3dd
Your Name <your.email@example.com>
2025-11-11 15:56:10 +0100

    revert change

--- src/main/java/com/example/enableerrorsignal/EnableErrorSignal/service/EmailReaderScheduler.java
+++ src/main/java/com/example/enableerrorsignal/EnableErrorSignal/service/EmailReaderScheduler.java
@@ -3,7 +3,6 @@ package com.example.enableerrorsignal.EnableErrorSignal.service;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class EmailReaderScheduler {
 
